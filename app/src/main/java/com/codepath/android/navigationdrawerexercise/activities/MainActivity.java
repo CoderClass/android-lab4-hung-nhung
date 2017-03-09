8ec0b79f83dea367b91fab537f6e3ee860e8a2ec
Hong Nhung <hongnhung@Hongs-MacBook-Pro.local>
2017-03-09 19:29:52 +0700

    pull code

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -2,11 +2,14 @@ package com.codepath.android.navigationdrawerexercise.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mTbMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
